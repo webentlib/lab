c6b5bc4f7eb5daf579603ed0acf754aa4e21065c
Maksim Koroteev <maksimkoroteev@ttk.ru>
2025-08-24 18:36:00 +0300

    yet another commit

--- js/select.js
+++ js/select.js
@@ -1,4 +1,5 @@
 export function Select(el) {
-    el.style.minWidth = Math.floor(el.offsetWidth) + 'px';
+    const FONT_EXPANSION_COEF = 2;
+    el.style.minWidth = Math.ceil(el.offsetWidth) + FONT_EXPANSION_COEF + 'px';
     el.classList.add('_STYLED');
 }
