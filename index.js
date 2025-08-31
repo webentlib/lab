fd31ffde9093b22622806bfb3ef30afcdfe1fb69
Maksim Koroteev <maksimkoroteev@ttk.ru>
2025-08-31 18:41:30 +0300

    yet another commit

--- index.js
+++
@@ -1,7 +0,0 @@
-import '/lab/js/extend.js';
-
-export { Helper } from '/lab/js/helper.js';
-export { Select } from '/lab/js/select.js';
-export { Datetime, DatetimeHelper } from '/lab/js/datetime.js';
-export { BaseModel } from '/lab/js/models.js';
-export { svg } from '/lab/js/svg.js';
---
+++ index.ts
@@ -0,0 +1,7 @@
+import '/lab/js/extend.ts';
+
+export { Helper } from '/lab/js/helper.ts';
+export { Select } from '/lab/js/select.ts';
+export { Datetime, DatetimeHelper } from '/lab/js/datetime.ts';
+export { BaseModel } from '/lab/js/models.ts';
+export { svg } from '/lab/js/svg.ts';
