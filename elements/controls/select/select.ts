74178a57b00e4158af2014484d33d5d21aaa20fe
Maksim Koroteev <maksimkoroteev@ttk.ru>
2025-10-28 20:50:59 +0300

    YAC

--- elements/controls/select/select.ts
+++
@@ -1,5 +0,0 @@
-export function Select(el) {
-    const FONT_EXPANSION_COEF = 2;
-    el.style.minWidth = Math.ceil(el.offsetWidth) + FONT_EXPANSION_COEF + 'px';
-    el.classList.add('_STYLED');
-}
--- scripts/models.ts
+++ scripts/models.ts
@@ -1,5 +1,6 @@
 export class BaseModel {
     constructor(data) {
+        if (!data) data = {};
         for (const [k, v] of Object.entries(data)) {
             this[k] = v
         }
