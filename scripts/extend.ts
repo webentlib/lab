3d80bac5987808afec737aacf538d403b1a4d06e
Maksim Koroteev <maksimkoroteev@ttk.ru>
2025-11-14 13:49:32 +0300

    YAC

--- scripts/extend.ts
+++ scripts/extend.ts
@@ -6,6 +6,10 @@ String.prototype.PAD = function(n=2) {
     return ('0' + this).slice(-n);
 }
 
+String.prototype.CAPITALIZE = function() {
+    return String(this).charAt(0).toUpperCase() + String(this).slice(1);
+}
+
 Number.prototype.FORMAT = function(n=' ') {
     // https://stackoverflow.com/a/41858572/4117781
     return this.toString().replace(/\B(?=(\d{3})+(?!\d))/g, n);
